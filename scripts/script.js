a2c01f14b87aa94fa0a5a967588573f4e7e6099a
Lorenzo <loreevil@gmail.com>
2021-09-13 17:40:03 +0200

    Aggiunta UI per inserire libri

--- scripts/script.js
+++ scripts/script.js
@@ -14,16 +14,93 @@ function slideIn(obj) {
     parent.style.width = "15vw";
     target.style.left = "12.6vw";
 
+    const insertForm = document.querySelector("#insertForm");
+    const insertPara = document.createElement("p");
+    const form = document.createElement("form");
+    const titleLabel = document.createElement("label");
+    const authorLabel = document.createElement("label");
+    const pagesLabel = document.createElement("label");
+    const trueRadioLabel = document.createElement("label");
+    const falseRadioLabel = document.createElement("label");
+    const inputTitle = document.createElement("input");
+    const inputAuthor = document.createElement("input");
+    const inputPages = document.createElement("input");
+    const inputRadioTrue = document.createElement("input");
+    const inputRadioFalse = document.createElement("input");
+    const addBtn = document.createElement("button");
+    const radioPara = document.createElement("p");
+
+    insertPara.textContent = "Provide neede data to add a new book.";
+    insertPara.setAttribute("id", "insertPara");
+
+    titleLabel.textContent = "Book Title";
+    titleLabel.setAttribute("for", "title");
+    titleLabel.setAttribute("id", "titleLabel");
+
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("placeholder", "Monogatari");
+    inputTitle.setAttribute("name", "title");
+
+    authorLabel.textContent = "Author";
+    authorLabel.setAttribute("for", "author");
+    authorLabel.setAttribute("id", "authorLabel");
+
+    inputAuthor.setAttribute("type", "text");
+    inputAuthor.setAttribute("placeholder", "Nisioisin");
+    inputAuthor.setAttribute("name", "author");
+
+    pagesLabel.textContent = "Pages";
+    pagesLabel.setAttribute("for", "pages");
+    pagesLabel.setAttribute("id", "pagesLabel");
+
+    inputPages.setAttribute("type", "text");
+    inputPages.setAttribute("placeholder", "250");
+    inputPages.setAttribute("name", "pages");
+
+    radioPara.textContent = "Have you read it?";
+    radioPara.setAttribute("id", "radioPara");
+
+    inputRadioTrue.setAttribute("type", "radio");
+    inputRadioTrue.setAttribute("name", "read");
+    inputRadioTrue.setAttribute("value", "true");
+
+    trueRadioLabel.textContent = "Read";
+    trueRadioLabel.setAttribute("for", "true");
+
+    inputRadioFalse.setAttribute("type", "radio");
+    inputRadioFalse.setAttribute("name", "read");
+    inputRadioFalse.setAttribute("value", "false");
+
+    falseRadioLabel.textContent = "Not Read Yet";
+    falseRadioLabel.setAttribute("for", "false");
+
+    addBtn.textContent = "Add Book";
+    addBtn.setAttribute("href", "#");
+    addBtn.setAttribute("id", "addBtn");
+
+    form.appendChild(titleLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(inputTitle);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(authorLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(inputAuthor);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(pagesLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(inputPages);
+    form.appendChild(radioPara);
+    form.appendChild(inputRadioTrue);
+    form.appendChild(trueRadioLabel);
+    form.appendChild(inputRadioFalse);
+    form.appendChild(falseRadioLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(addBtn);
+
+    insertForm.appendChild(insertPara);
+    insertForm.appendChild(form);
     /**
-     * inserire ricerca e aggiunta
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
+     * inserire ricerca
     */
 
     target.textContent = "<<";
@@ -38,17 +115,25 @@ function slideOut(obj) {
     parent.style.width = "3vw";
     target.style.left = "0.7vw";
 
+    const insertForm = document.querySelector("#insertForm");
+    
+    while(insertForm.hasChildNodes()) {
+        insertForm.removeChild(insertForm.firstChild);
+    }
     /**
-     * togliere ricerca e aggiunta
-     * 
-     * 
-     * 
-     * 
-     * 
+     * togliere ricerca
      */
 
     target.textContent = ">>";
     target.addEventListener("click", slideIn, {once: true});
+}
+
+function closeMoreInfo() {
+
+    const mainBody = document.querySelector("#mainBody");
+    const blur = document.querySelector("#blur");
+
+    mainBody.removeChild(blur);
 }
 
 function moreInfo(obj) {
@@ -83,6 +168,8 @@ function moreInfo(obj) {
     author.innerHTML = "<span class='aqua'>Author:</span> " + myLibrary[index].author;
     read.innerHTML = (myLibrary[index].read) ? "<span style='color:green;'>Read</span>" : "<span style='color:red;'>Not read yet</span>";
     cancel.innerHTML = "X";
+
+    cancel.addEventListener("click", closeMoreInfo);
 
     moreInfoWindow.appendChild(title);
     moreInfoWindow.appendChild(pages);
@@ -121,7 +208,8 @@ function addBookToLibrary(book) {
 
 let newBookIndex = 0;
 const myLibrary = [
-    new Book("Bakemonogatari", "Nisioisin", 250, false)
+    new Book("Bakemonogatari", "Nisioisin", 250, false),
+    new Book("Re:zero 1°volume", "Nagatsuki", 200, true)
 ];
 
 document.querySelector("#slidingBtn")
