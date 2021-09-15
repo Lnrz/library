5a24f1dbf6b9bfcedbc519720985262fd45a39da
Lorenzo <loreevil@gmail.com>
2021-09-15 17:56:56 +0200

    Aggiunto sistema di inserimento e rimozione libri e pulsante per aggiornare lo stato letto/non letto dei libri

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ function slideIn(obj) {
     const addBtn = document.createElement("button");
     const radioPara = document.createElement("p");
 
-    insertPara.textContent = "Provide neede data to add a new book.";
+    insertPara.textContent = "Provide needed data to add a new book.";
     insertPara.setAttribute("id", "insertPara");
 
     titleLabel.textContent = "Book Title";
@@ -70,6 +70,7 @@ function slideIn(obj) {
     inputRadioFalse.setAttribute("type", "radio");
     inputRadioFalse.setAttribute("name", "read");
     inputRadioFalse.setAttribute("value", "false");
+    inputRadioFalse.checked = true;
 
     falseRadioLabel.textContent = "Not Read Yet";
     falseRadioLabel.setAttribute("for", "false");
@@ -77,6 +78,8 @@ function slideIn(obj) {
     addBtn.textContent = "Add Book";
     addBtn.setAttribute("href", "#");
     addBtn.setAttribute("id", "addBtn");
+    addBtn.setAttribute("type", "button");
+    addBtn.addEventListener("click", addInputBook);
 
     form.appendChild(titleLabel);
     form.appendChild(document.createElement("br"));
@@ -148,34 +151,41 @@ function moreInfo(obj) {
     const author = document.createElement("p");
     const read = document.createElement("p");
     const cancel = document.createElement("p");
+    const remove = document.createElement("p");
 
     blur.setAttribute("id", "blur");
     moreInfoWindow.setAttribute("id", "moreInfoWindow");
+    moreInfoWindow.setAttribute("data-index", index);
     title.setAttribute("id", "titolo");
     pages.setAttribute("id", "pagine");
     author.setAttribute("id", "autore");
     read.setAttribute("id", "letto");
     cancel.setAttribute("id", "cancel");
+    remove.setAttribute("id", "remove");
 
     title.classList.add("infoP");
     pages.classList.add("infoP");
     author.classList.add("infoP");
     read.classList.add("infoP");
     cancel.classList.add("infoP");
+    remove.classList.add("infoP");
 
     title.innerHTML = "<span class='aqua'>Title:</span> " + myLibrary[index].title;
     pages.innerHTML = "<span class='aqua'>Pages:</span> " + myLibrary[index].pages;
     author.innerHTML = "<span class='aqua'>Author:</span> " + myLibrary[index].author;
     read.innerHTML = (myLibrary[index].read) ? "<span style='color:green;'>Read</span>" : "<span style='color:red;'>Not read yet</span>";
     cancel.innerHTML = "X";
+    remove.innerHTML = "Delete Book";
 
     cancel.addEventListener("click", closeMoreInfo);
+    remove.addEventListener("click", removeBook);
 
     moreInfoWindow.appendChild(title);
     moreInfoWindow.appendChild(pages);
     moreInfoWindow.appendChild(author);
     moreInfoWindow.appendChild(read);
     moreInfoWindow.appendChild(cancel);
+    moreInfoWindow.appendChild(remove);
     blur.appendChild(moreInfoWindow);
     mainBody.appendChild(blur);
 }
@@ -186,7 +196,8 @@ function addBookToLibrary(book) {
     const aBook = document.createElement("div");
     const title = document.createElement("p");
     const author = document.createElement("p");
-    const moreInfo = document.createElement("p");
+    const moreInfoD = document.createElement("p");
+    const readStatus = document.createElement("div");
 
     aBook.setAttribute("id", "aBook");
     aBook.setAttribute("data-index", newBookIndex);
@@ -195,15 +206,90 @@ function addBookToLibrary(book) {
     title.setAttribute("id", "title");
     author.textContent = book.author;
     author.setAttribute("id", "author");
-    moreInfo.textContent = "...";
-    moreInfo.setAttribute("id", "moreInfo");
+    moreInfoD.textContent = "...";
+    moreInfoD.setAttribute("id", "moreInfo");
+    moreInfoD.addEventListener("click", moreInfo);
+    readStatus.setAttribute("id", "readStatus");
+    readStatus.style.backgroundColor = ((book.read) ? "green" : "red");
+    readStatus.addEventListener("click", switchRead);
 
     aBook.appendChild(title);
     aBook.appendChild(author);
-    aBook.appendChild(moreInfo);
+    aBook.appendChild(moreInfoD);
+    aBook.appendChild(readStatus);
     bookContainer.appendChild(aBook);
 }
 
+function isValidBook(book) {
+    if ( book.title != "" && book.author != "" && book.pages > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+function addInputBook() {
+
+    const inputs = document.querySelectorAll("#insertForm input");
+    let newBook = new Book();
+
+    inputs.forEach(input => {
+        if (input.type != "radio" || input.checked) {
+            newBook[input.getAttribute("name")] = input.value;
+        }
+    });
+
+    newBook.read = (newBook.read == "true") ? true : false;
+
+    if (isValidBook(newBook)) {
+        
+        myLibrary.push(newBook);
+        addBookToLibrary(newBook);
+        inputs.forEach(input => {
+            if (input.type != "radio") {
+                input.value = "";
+            }
+        });
+
+    } else {
+        alert("Inserted data are not valid");
+    }
+}
+
+function switchRead(obj) {
+
+    let readStatus = obj.target;
+
+    readStatus.style.backgroundColor = 
+        (
+            (readStatus.style.backgroundColor == "red") ?
+            "green" : "red"
+        )
+    ;
+    
+    let index = readStatus.parentNode.getAttribute("data-index");
+
+    myLibrary[index].read = !myLibrary[index].read; 
+}
+
+function removeBook(obj) {
+
+    let index = obj.target.parentNode.getAttribute("data-index");
+
+    closeMoreInfo();
+
+    document.querySelectorAll("#aBook").forEach(book => {
+        if (+(book.getAttribute("data-index")) > index) {
+            book.setAttribute("data-index", +(book.getAttribute("data-index")) - 1)
+        } else if (+(book.getAttribute("data-index")) == index) {
+            book.remove();
+        }
+    })
+
+    myLibrary.splice(index, 1);
+
+    newBookIndex--;
+}
 ///////////////////////////////////////////////////////////////////////////////
 
 let newBookIndex = 0;
@@ -217,7 +303,3 @@ document.querySelector("#slidingBtn")
 ;
 
 myLibrary.forEach(book => addBookToLibrary(book));
-
-document.querySelectorAll("#moreInfo")
-    .forEach(x => x.addEventListener("click", moreInfo))
-;
