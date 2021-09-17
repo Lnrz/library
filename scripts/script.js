bdeba2f3b8a3c298a53ae652eea215385373f183
Lorenzo <loreevil@gmail.com>
2021-09-17 15:48:38 +0200

    Finito progetto

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,35 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
 function slideIn(obj) {
 
     let target = obj.target;
@@ -245,6 +274,7 @@ function addInputBook() {
         
         myLibrary.push(newBook);
         addBookToLibrary(newBook);
+        updateLocalStorage();
         inputs.forEach(input => {
             if (input.type != "radio") {
                 input.value = "";
@@ -270,6 +300,8 @@ function switchRead(obj) {
     let index = readStatus.parentNode.getAttribute("data-index");
 
     myLibrary[index].read = !myLibrary[index].read; 
+
+    updateLocalStorage();
 }
 
 function removeBook(obj) {
@@ -289,17 +321,19 @@ function removeBook(obj) {
     myLibrary.splice(index, 1);
 
     newBookIndex--;
+
+    updateLocalStorage();
 }
 ///////////////////////////////////////////////////////////////////////////////
 
-let newBookIndex = 0;
-const myLibrary = [
-    new Book("Bakemonogatari", "Nisioisin", 250, false),
-    new Book("Re:zero 1°volume", "Nagatsuki", 200, true)
-];
+let storageUsable = storageAvailable("localStorage");
+let newBookIndex = 0; 
+const myLibrary = (storageUsable && localStorage.length) ?
+     JSON.parse(localStorage.getItem("myLibrary")) :
+     []
+;
+myLibrary.forEach(book => addBookToLibrary(book));
 
 document.querySelector("#slidingBtn")
     .addEventListener("click", slideIn, {once: true})
 ;
-
-myLibrary.forEach(book => addBookToLibrary(book));
