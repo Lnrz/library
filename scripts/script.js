f8e3076030dd69f9038f5a2f2942641ef6b65c47
Lorenzo <loreevil@gmail.com>
2021-10-02 17:00:35 +0200

    Cambiata la funzione con la classe corrispondente

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function storageAvailable(type) {
